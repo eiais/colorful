43b778fbf2fa123af061ce7c5a2c3f2dcb95251c
Kathryn Spiers <kathryn@spiers.me>
2021-10-03 17:08:55 -0700

    replace optional color with an uncolored enum value

--- Sources/Colorful/Satisfiability.swift
+++ Sources/Colorful/Satisfiability.swift
@@ -8,12 +8,13 @@
 import Foundation
 import CCryptoBoringSSL
 
-typealias PartiallyColoredSATGraph = [UInt: (TruthColoring?, [UInt])]
+typealias PartiallyColoredSATGraph = [UInt: (TruthColoring, [UInt])]
 
 enum TruthColoring: UInt {
     case falseC = 0
     case truthC = 1
     case neutralC = 2
+    case uncolored = 3
 }
 
 func makeSATGraph() -> PartiallyColoredSATGraph {
@@ -23,25 +24,21 @@ func makeSATGraph() -> PartiallyColoredSATGraph {
 }
 
 func attachNot(to base: PartiallyColoredSATGraph, at point: UInt) -> PartiallyColoredSATGraph? {
-    let pColor, qColor: TruthColoring?
-    
-    if let (colorP, _) = base[point] as? (TruthColoring, [UInt]) {
-        let notResult : TruthColoring
-        switch colorP {
-        case .truthC:
-            notResult = .falseC
-        case .falseC:
-            notResult = .truthC
-        case .neutralC:
-            return nil
-        }
-        
-        (pColor, qColor) = (colorP, notResult)
-    } else {
-        (pColor, qColor) = (nil, nil)
+    guard let (colorP, _) = base[point] else {
+        return nil
     }
-    
-    let notGraph: PartiallyColoredSATGraph = [0: (pColor, [1, 2]), 1: (qColor, [0, 2]), 2: (TruthColoring.neutralC, [0, 1])]
+    let notResult : TruthColoring
+    switch colorP {
+    case .truthC:
+        notResult = .falseC
+    case .falseC:
+        notResult = .truthC
+    case .uncolored:
+        notResult = .uncolored
+    case .neutralC:
+        return nil
+    }
+    let notGraph: PartiallyColoredSATGraph = [0: (colorP, [1, 2]), 1: (notResult, [0, 2]), 2: (TruthColoring.neutralC, [0, 1])]
     return attachGraph(notGraph, to: base, matchingVertices: [0: point, 2: TruthColoring.neutralC.rawValue])
 }
 
@@ -49,27 +46,28 @@ func attachGraph(_ addition: PartiallyColoredSATGraph, to base: PartiallyColored
     var newBase = base
     let bMax = newBase.keys.max() ?? 0
     
-    for (vertexID, (possibleColorA, edges)) in addition {
+    for (vertexID, (colorA, edges)) in addition {
         let newEdges = edges.map { matchingVertices[$0] ?? $0 + bMax }
         
-        if let matchingVertex = matchingVertices[vertexID], let (possibleColorB, edgesB) = newBase[matchingVertex] {
-            let newPossibleColor : TruthColoring?
+        if let matchingVertex = matchingVertices[vertexID] {
+            guard let (colorB, edgesB) = newBase[matchingVertex] else {
+                return nil  
+            }
+            let newColor : TruthColoring
             
-            if let colorA = possibleColorA {
-                if let colorB = possibleColorB {
+            if colorA != .uncolored {
+                if colorB != .uncolored {
                     guard colorA == colorB else {
                         return nil
                     }
                 }
-                
-                newPossibleColor = possibleColorA
+                newColor = colorA
             } else {
-                newPossibleColor = possibleColorB
+                newColor = colorB
             }
-            
-            newBase[matchingVertex] = (newPossibleColor, edgesB + newEdges)
+            newBase[matchingVertex] = (newColor, edgesB + newEdges)
         } else {
-            newBase[vertexID + bMax] = (possibleColorA, newEdges)
+            newBase[vertexID + bMax] = (colorA, newEdges)
         }
     }
     
@@ -79,11 +77,7 @@ func attachGraph(_ addition: PartiallyColoredSATGraph, to base: PartiallyColored
 func coloredGraph(from partial: PartiallyColoredSATGraph) -> ColoredGraph? {
     var newGraph = ColoredGraph()
     
-    for (vertexID, (possibleColor, edges)) in partial {
-        guard let color = possibleColor else {
-            return nil
-        }
-        
+    for (vertexID, (color, edges)) in partial {
         let alteredColor : Colors
         switch color {
         case .falseC:
@@ -92,6 +86,8 @@ func coloredGraph(from partial: PartiallyColoredSATGraph) -> ColoredGraph? {
             alteredColor = .grn
         case .neutralC:
             alteredColor = .blu
+        case .uncolored:
+            return nil
         }
         
         newGraph[vertexID] = (alteredColor, edges)
